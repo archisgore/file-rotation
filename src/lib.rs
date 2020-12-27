cbe26fc24ca26191acc7390a9529d29958017a4b
Archis Gore <archis@polyverse.com>
2020-12-27 12:08:08 -0800

    Enable sync and async to not be mutually exclusive

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,7 @@
 pub mod error;
 
-#[cfg(not(feature = "async"))]
-mod synchronous;
-
-#[cfg(not(feature = "async"))]
-pub use synchronous::*;
+#[cfg(feature = "sync")]
+pub mod synchronous;
 
 #[cfg(feature = "async")]
-mod asynchronous;
-
-#[cfg(feature = "async")]
-pub use asynchronous::*;
+pub mod asynchronous;
